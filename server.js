a06979c2cefd25e68b84a86df52576b16b5a91fb
jdutton8 <jennydutton7@gmail.com>
2023-07-13 16:51:02 -0500

    added boilerplate js

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
 const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const noteData = require('./db/db');
+
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
