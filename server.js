7584f71d75cf2d1ea88b409ebf0baf3caedfb257
jdutton8 <jennydutton7@gmail.com>
2023-07-17 13:46:12 -0500

    got notes to write to db.json

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const noteData = require('./db/db');
+const {v4: uuidv4} = require("uuid");
 
 
 const PORT = 3001;
@@ -19,3 +20,52 @@ app.get("/", (req, res) => {
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 })
+
+app.get("/api/notes", (req, res) => {
+    console.info(`${req.method} request received to get notes`);
+    return res.json(JSON.parse(noteData));
+})
+
+app.post("/api/notes", (req, res) =>{
+    const {title, text} = req.body; 
+    
+    if(title && text){
+        
+        const newNote = {
+            title,
+            text, 
+            id: uuidv4()
+        };
+
+        fs.readFile("./db/db.json", "utf8", (err, data) =>{
+            if (err){
+                console.error(err);
+            }else{
+                const parsedNotes = JSON.parse(data);
+
+                parsedNotes.push(newNote);
+                const stringNotes = JSON.stringify(parsedNotes);
+
+                fs.writeFile("./db/db.json", stringNotes, (err) => err ? console.log(err) : console.log("success!"));
+            }
+        });
+
+        const response = {
+            status: 'success',
+            body: newNote,
+          };
+
+          console.log(response);
+          res.status(201).json(response);
+        } else {
+          res.status(500).json('Error in saving notes');
+        }  
+    
+})
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`Server listening on port ${PORT}!`)
+);
